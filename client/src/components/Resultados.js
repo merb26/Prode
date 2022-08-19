2e1a984aba145299b3aa408731ec3096c81cc52c
Agustin MIOTTI <amiotti@secco.local>
2022-08-19 16:25:24 -0300

    resultados component changes

--- client/src/components/Resultados.js
+++ client/src/components/Resultados.js
@@ -1,15 +1,10 @@
 import { useEffect, useState } from "react";
 import "../App.css";
 
-const testMatch = [
-  { id: 391881, winner: "Netherlands", goalHome: 1, goalAway: 2 },
-  { id: 391882, winner: "Ecuador", goalHome: 1, goalAway: 2 },
-];
-
 const apiToken = "cfccda3b57e4496d884919c349c9f8a7";
 
 const Resultados = (props) => {
-  const [results, setResults] = useState(0);
+  const [bets, setBets] = useState(0);
 
   async function fetchData() {
     try {
@@ -32,31 +27,69 @@ const Resultados = (props) => {
 
       const data = await response.json();
       console.log(data);
-      setResults(data);
+      setBets(data);
     };
     getData();
   }, []);
 
-  async function evaluateResponse() {
-    const userBet = await results.filter(
-      (result) => result.userId === props.id
-    );
-    console.log("USERBET", userBet);
+  const testMatch = [
+    { id: 391881, winner: "Netherlands", goalHome: 1, goalAway: 2 },
+    { id: 391882, winner: "Ecuador", goalHome: 1, goalAway: 2 },
+    { id: 391883, winner: "Argentina", goalHome: 1, goalAway: 2 },
+  ];
 
-    let aux;
+  const testBet = [
+    { id: 391881, winner: "Senegal", goalHome: 2, goalAway: 1, userId: 1 },
+    { id: 391882, winner: "Ecuador", goalHome: 1, goalAway: 2, userId: 1 },
+    { id: 391883, winner: "Argentina", goalHome: 1, goalAway: 3, userId: 1 },
+    { id: 391881, winner: "Netherlands", goalHome: 3, goalAway: 1, userId: 2 },
+    { id: 391882, winner: "Draw", goalHome: 2, goalAway: 2, userId: 2 },
+    { id: 391883, winner: "Argentina", goalHome: 2, goalAway: 3, userId: 2 },
+  ];
+
+  const evaluateUserResults = () => {
+    const obj = [];
+    let points = 0;
+
+    let newArray = [...new Set(testBet.map((bet) => bet.userId))];
+
+    for (let j = 0; j < bets.length; j++) {}
+
     for (let i = 0; i < testMatch.length; i++) {
-      // console.log(
-      //   "LOG",
-      //   await userBet.filter((el) => el.matchId == testMatch[i].id)[0]
-      // );
-      aux.push(await userBet.filter((el) => el.matchId == testMatch[i].id)[0]);
-      console.log("AUX", aux);
+      let caseWinner =
+        testMatch.filter((match) => match.id === testBet[i].id)[0].winner ===
+        testBet[i].winner;
+      let caseGoalHome =
+        testMatch.filter((match) => match.id === testBet[i].id)[0].goalHome ===
+        testBet[i].goalHome;
+      let caseGoalAway =
+        testMatch.filter((match) => match.id === testBet[i].id)[0].goalAway ===
+        testBet[i].goalAway;
+
+      if (caseWinner) {
+        points += 3;
+      }
+
+      if (caseGoalHome) {
+        points += 3;
+      }
+
+      if (caseGoalAway) {
+        points += 3;
+      }
+
+      //All
+      if (caseWinner && caseGoalHome && caseGoalAway) {
+        points += 10;
+      }
     }
-  }
+    console.log("POINTS", points);
+    return points;
+  };
 
   //console.log(Promise.all(evaluateResponse()));
 
-  return <div>{evaluateResponse}</div>;
+  return <div>{evaluateUserResults()}</div>;
 };
 
 export default Resultados;
